0e9f78261b10e5143519bfb2c18d75702fb18b1c
Scravt <114781058+Scravt@users.noreply.github.com>
2022-11-06 11:33:38 -0300

    Add files via upload

---
+++ cuentas.js
@@ -0,0 +1,31 @@
+//var cantidad=document.getElementById("cantidad");
+
+var total=document.getElementById("total11");
+var Valordetiket = 200
+var ValorConDescuento=0
+var cantidad = document.getElementById("cantidad");
+var $condicion = document.getElementById("inputState");
+
+function hola (){
+  total.innerHTML=ValorConDescuento*cantidad.value;
+  console.log(typeof(cantidad));
+}
+
+
+document.getElementById("resumen").onclick = () => {  
+  if ($condicion.selectedIndex == 1){
+      ValorConDescuento=Valordetiket*0.2;
+    } else if ($condicion.selectedIndex==2){
+      ValorConDescuento=Valordetiket*0.5;
+    }else if ($condicion.selectedIndex==3){
+      ValorConDescuento=Valordetiket*0.85;
+    }else{
+      ValorConDescuento=Valordetiket;
+    }
+
+    hola();
+}
+ 
+  
+
+  
